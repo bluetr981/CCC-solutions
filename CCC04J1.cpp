8ebd1ce1fc245895d8257f83ab7dae744da8ccc5
bluetr981 <152762210+bluetr981@users.noreply.github.com>
2023-12-03 14:44:12 -0500

    Create CCC04J1.cpp

---
+++ CCC04J1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    cout << "The largest square has side length" << " " << trunc(sqrt(n)) << "." << "\n";
+    return 0;
+}
